c9a7c8008f1beaa1819088bc81f0b02fb0e24003
Alexander <kisliy.doma@gmail.com>
2024-09-10 18:21:08 +0800

    adds MiniCssExtractPlugin for production mode

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,7 @@
 import { Configuration, ProgressPlugin } from 'webpack';
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 
 enum EMode {
@@ -11,13 +12,14 @@ type TEnv = { mode: EMode; port: number };
 
 export default (env: TEnv) => {
   const isDev = env.mode === EMode.DEVELOPMENT;
+  const isProd = env.mode === EMode.PRODUCTION;
 
   const config: Configuration = {
     mode: env.mode ?? EMode.DEVELOPMENT,
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
     output: {
       path: path.resolve(__dirname, 'build'),
-      filename: 'bundle.[contenthash].js',
+      filename: 'js/bundle.[contenthash].js',
       clean: true,
     },
     plugins: [
@@ -25,12 +27,19 @@ export default (env: TEnv) => {
         template: path.resolve(__dirname, 'public', 'index.html'),
       }),
       isDev && new ProgressPlugin(),
+      isProd &&
+        new MiniCssExtractPlugin({
+          filename: 'css/style.[contenthash].css',
+          chunkFilename: 'css/style.[contenthash].css',
+        }),
     ].filter(Boolean),
     module: {
       rules: [
         {
           test: /\.s[ac]ss$/i,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
+          use: [isDev && 'style-loader', isProd && MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'].filter(
+            Boolean,
+          ),
         },
         {
           test: /\.tsx?$/,
